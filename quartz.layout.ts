d1b7192830839e65d6c68b5dd0814f00e1093328
Mamiglia <matteo.migliarini.01@gmail.com>
2024-11-01 19:44:46 +0000

    Quartz sync: Nov 1, 2024, 7:44 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,21 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [  Component.Comments({
+    provider: 'giscus',
+    options: {
+      // from data-repo
+      repo: 'Mamiglia/notes',
+      // from data-repo-id
+      repoId: 'R_kgDONJK4AA',
+      // from data-category
+      category: 'Comments',
+      // from data-category-id
+      categoryId: 'DIC_kwDONJK4AM4Cj5oV',
+    }
+  }),	
+	
+	],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
